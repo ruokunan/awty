953cb4ece3614c4b08aa219f1d3b62069cb4c503
Ruokun An <ruokua@cs.washington.edu>
2015-05-21 10:45:37 -0700

    Update AlarmReceiver.java

--- app/src/main/java/edu/awty/ruokua/arewethere/AlarmReceiver.java
+++ app/src/main/java/edu/awty/ruokua/arewethere/AlarmReceiver.java
@@ -9,12 +9,20 @@ import android.widget.Toast;
  * Created by ruokua on 5/13/15.
  */
 public class AlarmReceiver extends BroadcastReceiver {
-    private static final String MESSAGE = "message";
-
+    public static final String MESSAGE = "message";
+    public static final String PHONE_NUMBER = "phoneNumber";
     @Override
     public void onReceive(Context context, Intent intent) {
         assert (intent != null);
+        
         String message = intent.getStringExtra(MESSAGE);
+        
+        String phone = intent.getStringExtra(PHONE_NUMBER);
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage(phone, null, msg, null, null);
+
+       
+       
         Toast.makeText(context, message, Toast.LENGTH_LONG).show();
     }
 
